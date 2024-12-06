52129fba9bb57b704a260cf7bb4b369f1bb4c7d0
tanishqtote55 <tanishqtote.55@gmail.com>
2024-12-06 14:02:00 +0530

    Defined Node Structure for Linked List to Store Tourist Spot Details.

--- header.h
+++ header.h
@@ -10,3 +10,16 @@
 
 // Maximum length for city or spot names
 #define MAX_CITY_NAME 100
+
+// Definition of a node in the singly linked list
+typedef struct node {
+    char cityName[MAX_CITY_NAME];  // City name
+    char spotName[MAX_CITY_NAME];  // Tourist spot name
+    float rating;                  // Rating of the tourist spot
+    char Longitude[20];            // Longitude of the spot
+    char Latitude[20];             // Latitude of the spot
+    struct node* next;             // Pointer to the next node
+} node;
+
+// Typedef for simplicity
+typedef node *SLL;
