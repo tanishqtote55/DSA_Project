df08088a8865066d0e1f179a296a739e9f84f11e
tanishqtote55 <tanishqtote.55@gmail.com>
2024-12-06 14:08:00 +0530

    Added Comments in length Function

--- logic.c
+++ logic.c
@@ -4,6 +4,8 @@
 int length(SLL head){
     int count = 0;
     node *temp = head;
+
+    // Traverse the list to count the number of nodes
     while(temp){
         count++;
         temp = temp -> next;
