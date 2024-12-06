f59d40a867aff47abb2a14964f5474c5fc7ab555
tanishqtote55 <tanishqtote.55@gmail.com>
2024-12-06 14:12:24 +0530

    main.c Updated

--- main.c
+++ main.c
@@ -1,69 +1,8 @@
 #include "header.h"
 
-void toLowerCase(char *str) {
-    for (int i = 0; str[i]; i++) {
-        str[i] = tolower(str[i]);  
-    }
-}
-
-void displayTouristSpots(const char* cityName){
-    FILE* file = fopen("./csv/tourist_spots.csv", "r");
-    if(file == NULL){
-        printf("Error Opening File\n");
-        return;
-    }
-    char line[MAX_LINE_LENGTH];
-    char *token;
-
-    fgets(line, sizeof(line), file);
-
-    while(fgets(line, sizeof(line), file)){
-        char city[MAX_CITY_NAME];
-        char spotName[100], type[50], longitude[20], latitude[20];
-        float rating;
-
-        token = strtok(line, ","); 
-        strcpy(city, token);
-
-        token = strtok(NULL, ",");
-        strcpy(spotName, token);
-
-        token = strtok(NULL, ",");
-        rating = atof(token);
-
-        token = strtok(NULL, ",");
-        strcpy(type, token); 
-
-        token = strtok(NULL, ",");
-        strcpy(longitude, token);
-
-        token = strtok(NULL, ",");
-        strcpy(latitude, token);
-
-        latitude[strcspn(latitude, "\n")] = '\0';
-        char cityLower[MAX_CITY_NAME];
-        strcpy(cityLower, city);
-        toLowerCase(cityLower);
-        char cityNameLower[MAX_CITY_NAME];
-        strcpy(cityNameLower, cityName);
-        toLowerCase(cityNameLower);
-
-        if(strcmp(cityLower, cityNameLower) == 0){
-            printf("%s", city);
-            printf(" %s", spotName);
-            printf(" %.2f", rating);
-            printf(" %s", type);
-            printf(" %s", longitude);
-            printf(" %s\n", latitude);
-        }
-    }
-    fclose(file);
-}
-
 int main(){
     char cityName[MAX_CITY_NAME];
     printf("Enter the city name: ");
     scanf("%99s", cityName);
-    displayTouristSpots(cityName);
     return 0;
 }
